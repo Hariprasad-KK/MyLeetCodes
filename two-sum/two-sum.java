fb847dd42400ed705c669462809abd332b6109b4
Hariprasad K K <56286476+Hariprasad-KK@users.noreply.github.com>
2021-09-16 19:56:52 +0530

    Time: 96 ms (9.16%), Space: 41.9 MB (17.90%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -3,14 +3,9 @@ class Solution {
         for(int i=0;i<nums.length;i++)
         {
             for(int j=i+1;j<nums.length;j++)
-            {
-                int complement= target-nums[i];
+            {int complement= target-nums[i];
                 if(nums[j]==complement)
-                {
-                    return new int[]{i,j};
-                }
-            }
-        }
-        throw new IllegalArgumentException("no match found");
+                { return new int[]{i,j};
+                }}} throw new IllegalArgumentException("no match found");
     }
 }
