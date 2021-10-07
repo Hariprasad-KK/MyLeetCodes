66c24e1e1f5a5d43bb39855eb668122b019b19e3
Hariprasad K K <56286476+Hariprasad-KK@users.noreply.github.com>
2021-10-07 07:27:00 +0530

    Time: 1 ms (100.00%), Space: 35.9 MB (80.50%) - LeetHub

---
+++ sqrtx/sqrtx.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0)
+        {
+            return 0;
+        }
+        int start=1 ;
+            int end=x;
+            int ans=0;
+        while(start<=end)
+        {
+            int mid= start+(end-start)/2;
+            if(mid<=x/mid)
+            {
+                ans=mid;
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+}  
