2217e7bdf3cd09a1f2a24854fe4dbdf04dacb9db
Hariprasad K K MAD <56286476+Hariprasad-KK@users.noreply.github.com>
2022-07-16 10:21:08 +0530

    Time: 6 ms (88.55%), Space: 43.2 MB (34.28%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] mappingDT=new int[256];
+        Arrays.fill(mappingDT,-1);
+        
+        int[] mappingDS = new int[256];
+        Arrays.fill(mappingDS,-1);
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char c1=s.charAt(i);
+            char c2=t.charAt(i);
+            
+            if(mappingDT[c1]==-1 && mappingDS[c2]==-1)
+            {
+                mappingDT[c1]=c2;
+                mappingDS[c2]=c1;
+            }
+            else if (!(mappingDT[c1] == c2 && mappingDS[c2]==c1))
+            {
+                 
+                return false;
+                
+            }
+        }
+        
+        return true;
+    } 
+}
