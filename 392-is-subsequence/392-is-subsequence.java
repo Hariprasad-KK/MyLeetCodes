5a874b3a22697d079c78feb99db452800d737b21
Hariprasad K K MAD <56286476+Hariprasad-KK@users.noreply.github.com>
2022-07-24 21:11:41 +0530

    Time: 2 ms (46.82%), Space: 42 MB (49.50%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length()==0)
+        {
+            return true;
+            
+        }
+        if(s.length() > t.length())
+        {
+            return false;
+        }
+        
+    int index=0;
+        for(int i=0;i<t.length();i++)
+        {
+            if(t.charAt(i)==s.charAt(index))
+            {
+                index++;
+            }
+            if(index==s.length())
+            {
+                return true;
+            }
+        }
+  return false;
+    }
+    
+}
